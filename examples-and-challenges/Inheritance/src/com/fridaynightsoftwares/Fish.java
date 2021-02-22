cef67a75a7207a7f81d46947c6990f7ee3e2ccc4
Gabriel Alvares Gibran <gagibran@gmail.com>
2021-02-22 20:26:43 -0300

    Finished Lesson 82

---
+++ examples-and-challenges/Inheritance/src/com/fridaynightsoftwares/Fish.java
@@ -0,0 +1,38 @@
+package com.fridaynightsoftwares;
+
+public class Fish  extends Animal {
+
+    // Fields:
+    private int fins;
+    private int gills;
+    private String scales;
+    private int eyes;
+    private int teeth;
+
+    // Constructor with Animal constructor:
+    public Fish(String name, int size, int weight, int fins, int gills, String scales, int eyes, int teeth) {
+        super(name, 1, 1, size, weight);
+        this.fins = fins;
+        this.gills = gills;
+        this.scales = scales;
+        this.eyes = eyes;
+        this.teeth = teeth;
+    }
+
+    // Generic methods:
+    public void swim(int speed) {
+        System.out.println(super.getName() + " has started swimming.");
+        super.move(speed);
+    }
+    public void rest(int time) {
+        System.out.println(super.getName() + " rests for " + time + " minutes.");
+    }
+
+    // Overridden methods:
+
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println("Nhac!");
+    }
+}
--- examples-and-challenges/Inheritance/src/com/fridaynightsoftwares/Main.java
+++ examples-and-challenges/Inheritance/src/com/fridaynightsoftwares/Main.java
@@ -7,5 +7,8 @@ public class Main {
         Dog luna = new Dog("Luna", 8, 20, "Short", 2, 4, 1, 32);
         luna.eat(); // Calling a method from the Animals class.
         luna.walk();
+        Fish shark = new Fish("Shark", 10, 30, 10, 4, "Sharp", 2, 40);
+        shark.swim(40);
+        shark.eat();
     }
 }
