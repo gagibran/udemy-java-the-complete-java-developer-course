6c90b76651e42458a9f30bc44b1c0983fa0b9b87
Gabriel Alvares Gibran <gagibran@gmail.com>
2021-03-09 19:14:56 -0300

    Finished Coding Exercise 46

---
+++ examples-and-challenges/LinkedListChallenge/src/com/fridaynightsoftwares/Album.java
@@ -0,0 +1,51 @@
+package com.fridaynightsoftwares;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+    public Album(String name, String artist) {
+        songs = new ArrayList<>();
+        this.name = name;
+        this.artist = artist;
+    }
+    public boolean addSong(String title, double duration) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return false;
+            }
+        }
+        songs.add(new Song(title, duration));
+        return true;
+    }
+    private Song findSong(String title) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+        if (trackNumber > songs.size() || trackNumber <= 0) {
+            return false;
+        }
+        Song songToBeAdded = songs.get(trackNumber - 1);
+        if (findSong(songToBeAdded.getTitle()) != null) {
+            playlist.add(songToBeAdded);
+            return true;
+        }
+        return false;
+    }
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song songToBeAdded = findSong(title);
+        if (songToBeAdded != null) {
+            playlist.add(songToBeAdded);
+            return true;
+        }
+        return false;
+    }
+}
